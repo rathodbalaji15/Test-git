332201d2a9563b4c7984c263cc7d89bc19d43b2c
balajirathod <rathodbalaji123@gmail.com>
2023-11-05 21:06:35 +0530

    This is second commit from rebase_demo

--- Test-project/src/com/test/Message.java
+++ Test-project/src/com/test/Message.java
@@ -3,6 +3,7 @@ package com.test;
 public class Message {
     public void name(String name){
         System.out.println(name);
+        System.out.println("This is second commit from rebase_demo");
 
 
 
