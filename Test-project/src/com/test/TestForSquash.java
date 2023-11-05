6c9612d9bc8774070f102287829d9c29676918c5
balajirathod <rathodbalaji123@gmail.com>
2023-11-05 20:54:27 +0530

    This is first commit from rebase_demo

--- Test-project/src/com/test/TestForSquash.java
+++ Test-project/src/com/test/TestForSquash.java
@@ -3,5 +3,7 @@ package com.test;
 public class TestForSquash {
     public void getSquash(){
         System.out.println("This is git squash");
+        System.out.println("This is commi for rebase_demo branch");
+
     }
 }
